22833da6e7ab7e469d63ae7c97041114002dfbb1
18639710721 <2634875948@qq.com>
2023-03-06 19:20:30 +0800

    Add base template and error template

---
+++ FlaskQuickStartOptimizeTemplatesV5/app.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/3/5 22:02
+# @Author  : listem
+# @FileName: app.py
+# @Software: PyCharm
+"""
+使用指令创建数据库
+    flask initdb
+    flask --help 查看flask指令
+    查询的时候要引入 from app import User, Movie, db
+
+模板优化
+    定义404页面
+    模板继承
+
+"""
+import click
+import os
+import sys
+
+from flask import Flask, render_template  # 生产视图函数对应的URL
+from flask_sqlalchemy import SQLAlchemy
+
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
+
+# 实例化Flask类 创建一个程序对象app
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
+print(os.path)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
+"""
+使用路由装饰器来为这个函数绑定对应的 URL
+触发这个函数获取返回值，并把返回值显示到浏览器窗口
+    第一个参数是URL规则字符串 这里的/指的是根地址
+"""
+
+
+# 生成一些虚拟数据
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+    # 全局的两个变量移动到这个函数内
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done')
+
+
+# 模板上下文处理函数
+@app.context_processor
+def inject_user():  # 函数名可以随意修改
+    user = User.query.first()
+    return dict(user=user)  # 需要返回字典, 等同于 return {'user': user}
+
+
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    return render_template('404.html'), 404
+
+
+@app.route('/')
+def index():
+    movies = Movie.query.all()  # 读取所有电影记录
+    return render_template('index.html', movies=movies)  # 通过关键字参数传入变量
+
+
+@app.cli.command('initdb')  # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=7777,
+        debug=True
+    )
---
+++ FlaskQuickStartOptimizeTemplatesV5/doc.md
@@ -0,0 +1,2 @@
+原项目文档路径
+"C:\E\编程\Flask\Flask-tutorial\"
